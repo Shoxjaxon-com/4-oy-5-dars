4da9da96ce7f7f405931565b2190b2239189b6a7
Shoxjaxon-com <alijonovshohjahon03@gmail.com>
2024-07-24 09:50:22 -0700

    vazifa

---
+++ index.js
@@ -0,0 +1,134 @@
+// 1-masala
+// let arr = [ 4,15, 20 ,30,90]
+
+// let res =[]
+// for( let i =0; i < arr.length; i++){
+//     res.push(arr[i]*2)
+// }console.log(res);
+
+// // 2-masala
+// let arr = [ 4,15, 20 ,30,90]
+// let res =[]
+// for( let i =0; i < arr.length; i++){
+//     res.unshift(arr[i]*2)
+// }console.log(res);
+
+// 3-masala
+// let arr = [ 4,-15, -20 ,30,90]
+// let res =[]
+// for( let i =0; i < arr.length; i++){
+//     if(arr[i] > 0){
+//         res.push(arr[i])
+//     }
+// }console.log(res);
+
+// 4-masala
+// let arr = [ 5, 10, 15, 20]
+// let sum = 0;
+// for( let i = 0; i< arr.length; i++){
+//     sum = sum+arr[i]
+// }
+// console.log(sum);
+
+// 5-masala
+
+// let arr = [35,15,5]
+// arr[2] = 8;
+// console.log(arr);
+
+// 6-masala
+
+// let num= [1, 2, 3, 4]
+// num.pop();
+// console.log(num);
+
+// 7-masala
+// let num= [1, 2, 3, 4]
+// num.shift();
+// console.log(num);
+
+// 8 masala
+// / let num= [1, 2, 3, 4]
+// num.unshift("234")
+
+
+//  9 masala
+// / let num= [1, 2, 3, 4]
+
+// for(let i=0; i < num.length; i++){
+//    console.log(i);
+// }
+// 10-masala
+
+// let arr = [ 1,2,3,4,5]
+// let num =[...arr]
+// console.log(num);
+
+// 11-masala
+
+// let arr = [1,2,3,4,5,6]
+// let max = Math.max(...arr);
+// console.log(max);
+
+// 12-masala
+// let arr = [1,2,3,4,5]
+// let min = Math.min(...arr)
+// console.log(min);
+
+// 13 masala
+//  let num= [1, 2, 3, 4]
+//  let res=0;
+// for(let i=0; i < num.length; i++){
+//    if (num[i] % 2 == 0) {
+//          counter=num[i]
+//    }
+// }
+
+// console.log(counter);
+// 14 masala
+//  let num= [1, 2, 3, 4]
+//  let counter=0;
+// for(let i=0; i < num.length; i++){
+//    if (num[i] % 2 == 1) {
+//          counter=num[i]
+//    }
+// }
+// console.log(counter);
+// 15 masala
+// let num= [1, 0, 3, 5, 6]
+// if (num==0) {
+//     num.pop()
+// }
+// console.log(num);
+
+// 18-masala
+// let arr = [1,2,3,4,5]
+// let res = 1;
+// for( let i =0; i<arr.length; i++){
+//     res*=arr[i]
+// }
+// console.log(res);
+// 19 masala
+//  let num= [1, 2, 3, 4]
+//  let counter=0;
+// for(let i=0; i < num.length; i++){
+//    if (num[i] % 2 == 1) {
+//     counter= num[i]
+// }
+// }
+// console.log(counter);
+// 20 masala
+//  let num= [1, 2, 3, 4]
+//  let counter=0;
+// for(let i=0; i < num.length; i++){
+//    if (num[i] % 2 == 0) {
+//     counter= num[i]
+// }
+// }
+// console.log(counter);
+
+
+
+// 23 masala
+ let num= [1, 2, 3, 4]
+console.log(num.length);
